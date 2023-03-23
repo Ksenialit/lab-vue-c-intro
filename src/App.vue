aa6a2bcc6c7cf2dfbf5e9f56b4f375ffb407e0b1
Ksenia Litovkina <kseniaa22@gmail.com>
2023-03-23 22:08:11 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
 <template>
-  <h1>Hola</h1>
+  <NavBar />
+  <main>
+    <section>
+      <img>
+      <h1></h1>
+      <h3></h3>
+      <button>Awesome!</button>
+    </section>
+  </main>
 </template>
 
+<script>
+import NavBar from '...'
+export default {
+  name: 'App',
+  
+}
+
+</script>
+
 <style scoped>
+//estilo de de todo el template exepto los que sean componentes
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <img src="" alt="ironhack logo">
+        <img src="" alt="ironhack menu">
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+
+<style scoped>
+nav {
+    display: flex;
+    justify-content: space-between
+
+}
+</style>
